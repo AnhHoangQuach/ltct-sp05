d845f83043f4e8cb44263966c9320398158a25e3
AnhHoangQuach <hoanganh36.work@gmail.com>
2022-01-23 00:45:41 +0700

    feat: add full api for setting product

--- controllers/SettingProductController.js
+++ controllers/SettingProductController.js
@@ -1,17 +1,60 @@
 const SettingProduct = require('../models/SettingProduct')
 
+module.exports.getAll = async (req, res, next) => {
+  try {
+    const settings = await SettingProduct.find()
+    return res.status(200).json({ status: 'success', data: settings })
+  } catch (error) {
+    return res.status(500).json({ status: 'error', message: error.message })
+  }
+}
+
 module.exports.create = async (req, res, next) => {
   try {
-    const categoryExist = await SettingProduct.findOne({ name: req.body.name })
-    console.log(categoryExist)
-    if (categoryExist) {
-      return res.status(404).json({ status: 'error', message: 'Setting Product Category Exist' })
+    const productSettingExist = await SettingProduct.findOne({ value: req.body.value })
+    if (productSettingExist) {
+      return res.status(404).json({ status: 'error', message: 'Value Setting Product Exist' })
     }
 
-    const newCategory = await SettingProduct.create({ name: req.body.name })
-    return res
-      .status(200)
-      .json({ status: 'success', data: newCategory, message: 'Setting Product Category Created' })
+    const productSetting = await SettingProduct.create(req.body)
+    return res.status(200).json({
+      status: 'success',
+      data: productSetting,
+      message: 'Setting Product Created',
+    })
+  } catch (error) {
+    return res.status(500).json({ status: 'error', message: error.message })
+  }
+}
+
+module.exports.updateSetting = async (req, res, next) => {
+  const { settingProductId } = req.params
+  const { value, categories } = req.body
+  try {
+    const settingProduct = await SettingProduct.findOne({ _id: settingProductId })
+    settingProduct.value = value
+    settingProduct.categories = categories
+
+    const afterSetting = await settingProduct.save()
+    return res.status(200).json({
+      status: 'success',
+      data: afterSetting,
+      message: 'Update Setting Product',
+    })
+  } catch (error) {
+    return res.status(500).json({ status: 'error', message: error.message })
+  }
+}
+
+module.exports.deleteSetting = async (req, res, next) => {
+  const { settingProductId } = req.params
+  try {
+    await SettingProduct.deleteOne({ _id: settingProductId })
+    return res.status(200).json({
+      status: 'success',
+
+      message: 'Deleted Setting Product',
+    })
   } catch (error) {
     return res.status(500).json({ status: 'error', message: error.message })
   }
--- routes/settingProduct.js
+++ routes/settingProduct.js
@@ -1,8 +1,19 @@
 const express = require('express')
 const settingProductRouter = express.Router()
 
-const { create } = require('../controllers/SettingProductController')
+const {
+  create,
+  getAll,
+  deleteSetting,
+  updateSetting,
+} = require('../controllers/SettingProductController')
+
+settingProductRouter.get('/', getAll)
 
 settingProductRouter.post('/create', create)
+
+settingProductRouter.delete('/:settingProductId', deleteSetting)
+
+settingProductRouter.patch('/:settingProductId', updateSetting)
 
 module.exports = settingProductRouter
